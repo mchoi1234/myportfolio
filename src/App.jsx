38dac1c18c1bd92793f6fc865fbb498c70374c2a
Minjoo <minjoo.choi12345@gmail.com>
2024-11-18 00:06:23 -0800

    Commit changes before deployment

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
   const [finishedScrolling, setfinishedScrolling] = useState(false)
 
   return (
-       <Router>
+    <Router basename="/myportfolio">
       <Routes>
         <Route path="/" element={<MainPage />} /> {/* Your main page */}
         <Route path="/resume" element={<ResumePage />} /> {/* Resume page */}
