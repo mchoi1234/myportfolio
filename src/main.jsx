747fa9acbbb12a0a3ba3b3a2929ad17c06c589be
Minjoo <minjoo.choi12345@gmail.com>
2024-11-18 11:39:01 -0800

    Configure React app for deployment to GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+const root =createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    root.render(<App />);
   </StrictMode>,
 )
