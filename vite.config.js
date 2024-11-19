36086d120969fca0615564ae9d9380caa8608f9a
Minjoo <minjoo.choi12345@gmail.com>
2024-11-18 18:22:01 -0800

    Updates

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: './myportfolio/',  // This ensures all paths are relative to /myportfolio/
-  plugins: [react()]
-
-})
